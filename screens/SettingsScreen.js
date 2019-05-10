67e97d2ea53b313947e90d465d4be0d1bd2e0f06
Zachary Centracchio <ZCentracchio@vt.ewsd.org>
2019-05-10 08:56:06 -0400

    first commit

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ExpoConfigView, View, Text } from '@expo/samples';
+
+export default class SettingsScreen extends React.Component {
+  static navigationOptions = {
+    title: 'app.json',
+  };
+
+  render() {
+    <View>
+      <Text>1</Text>
+      <Text>2</Text>
+      <Text>3</Text>
+      <Text>4</Text>
+    </View>
+  }
+}
