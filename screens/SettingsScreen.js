e1dcea5b663f4ec6a7b8fa92b4970231bb21bad9
Zachary Centracchio <ZCentracchio@vt.ewsd.org>
2019-05-22 10:39:27 -0400

    whatever

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,17 +1,90 @@
-import React from 'react';
-import { ExpoConfigView, View, Text } from '@expo/samples';
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet, Image, TouchableHighlight, ImageBackground } from 'react-native';
+import { Constants } from 'expo';
 
-export default class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'app.json',
-  };
-
-  render() {
-    <View>
-      <Text>1</Text>
-      <Text>2</Text>
-      <Text>3</Text>
-      <Text>4</Text>
-    </View>
-  }
+export default class App extends Component {
+    
+    state = {
+        backgroundToDisplay: '',
+    }
+    
+    selectBackground = (timeOfDay) => {
+        if (timeOfDay == 'morning') {
+            this.setState({ 
+                backgroundToDisplay: '../assets/images/robot-dev.png',
+            })
+        } else if (timeOfDay == 'afternoon') {
+            this.setState({ 
+                backgroundToDisplay: 'https://spliffmobile.com/download/new-york-5896.jpg',
+            })
+        } else if (timeOfDay == 'evening') {
+            this.setState({ 
+                backgroundToDisplay: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/new-york-city-empire-state-building-sunset-christopher-arndt.jpg',
+            })
+        }
+    }
+    
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground
+                    style={styles.background}
+                    source={this.state.backgroundToDisplay}
+                >
+                
+                    <TouchableHighlight
+                        onPress={() => this.selectBackground('morning')}
+                    >
+                        <View style={styles.buttonView}>
+                            <Text style={styles.buttonText}>
+                                Good morning!
+                            </Text>
+                        </View>
+                    </TouchableHighlight>
+                    
+                    <TouchableHighlight
+                        onPress={() => this.selectBackground('afternoon')}
+                    >
+                        <View style={styles.buttonView}>
+                            <Text style={styles.buttonText}>
+                                Good afternoon!
+                            </Text>
+                        </View>
+                    </TouchableHighlight>
+                    
+                    <TouchableHighlight
+                        onPress={() => this.selectBackground('evening')}
+                    >
+                        <View style={styles.buttonView}>
+                            <Text style={styles.buttonText}>
+                                Good evening!
+                            </Text>
+                        </View>
+                    </TouchableHighlight>
+                
+                </ImageBackground>
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 1,
+    },
+    buttonView: {
+        height: 30,
+        width: 100,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        marginBottom: 3,
+    },
+    buttonText: {
+        fontSize: 10, 
+        color: 'white',
+        textAlign: 'center',
+    },
+});
