9e03161d4c409cca78570fa57dcfbd3d00f403c0
jveilleux <jveilleux@vt.ewsd.org>
2019-05-24 08:50:09 -0400

    Viewnotworking

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -10,8 +10,12 @@ export default class LinksScreen extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
+        <View>
+          <Text>Reciepts:</Text>
+          <Text>Bills:</Text>
+          <Text>Entertainment:</Text>
+          <Text>Food:</Text>
+        </View>
         <ExpoLinksView />
       </ScrollView>
     );
