93411df7e82b6f8952f7bef2979c66a98d02b318
jveilleux <jveilleux@vt.ewsd.org>
2019-05-29 09:14:25 -0400

    workingSecondScreen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,23 +1,39 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View, Text, TextInput, ImageBackground } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { white } from 'ansi-colors';
 
 export default class LinksScreen extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+        inputValue: "Input Text!",
+    }
+}
   static navigationOptions = {
-    title: 'Links',
+    title: 'Receipts',
   };
-
+  _handleTextChange = inputValue => {
+    this.setState({ inputValue });
+};
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <View>
+        <View style={styles.container}>
+        <ImageBackground
+         style={styles.imgBackground}
+                    source={require('../assets/images/receipts-Screen.png')}
+                >
+        <TextInput
+                    value={this.state.inputValue}
+                    onChangeText={this._handleTextChange}
+                    style={{ width: 250, height: 44, padding: 8, borderColor: 'darkblue', borderWidth: 1, marginTop: 10, color: 'black'}}
+                />
           <Text>Reciepts:</Text>
           <Text>Bills:</Text>
           <Text>Entertainment:</Text>
           <Text>Food:</Text>
+          </ImageBackground>
         </View>
-        <ExpoLinksView />
-      </ScrollView>
     );
   }
 }
@@ -27,5 +43,10 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
+    color: 'white',
   },
+  imgBackground:{
+    alignItems: 'center',
+    flex: 1,
+},
 });
