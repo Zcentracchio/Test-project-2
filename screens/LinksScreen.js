b4f659c27564074226b1d2cd77f6b6006adcaf01
Lea Ann <lsmith@ewsd.org>
2019-06-02 11:44:30 -0400

    Next try at using mapContextToProps

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,40 +1,26 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import {mapContextToProps} from '../components/AppContext';
 
-class LinksScreen extends React.Component {
-  static navigationOptions = {
-      title: 'app.json',
-  };
 
-export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
+
+  export default class LinksScreen extends React.Component {
+    static navigationOptions = {
+      title: 'Links',
+    };
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        <Text>Hi</Text>
       </ScrollView>
     );
   }
 }
 
-export default mapContextToProps(LinksScreen);
-const styles = {
-    dark: {
-        backgroundColor: 'black',
-        color: 'white',
-    },
-    light: {
-        backgroundColor: 'white',
-        color: 'black',
-    },
-};
+
+
 
 const styles = StyleSheet.create({
   container: {
