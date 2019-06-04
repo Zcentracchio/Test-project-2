3ec208ebc5d1453f33154ed688eff68d16f17414
Lea Ann <lsmith@ewsd.org>
2019-06-04 09:54:59 -0400

    fixed conflicts setting screen, hopefully for the last time

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,50 +1,9 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { AppRegistry, View, StyleSheet, Text, ImageBackground, TouchableHighlight } from 'react-native';
-=======
 import { AppRegistry, Text, View, StyleSheet, Image, TouchableHighlight, ImageBackground, Button} from 'react-native';
->>>>>>> 37224bdb8225b3b65ac5b0bb0c906ca30fa82bf0
 import { Constants } from 'expo';
 
 export default class App extends Component {
     
-<<<<<<< HEAD
-    state = {
-        currentImg: ['http://www.designbolts.com/wp-content/uploads/2015/07/Colorfu-Polygon-iphone-6-background.jpg', 'https://i.pinimg.com/originals/24/fb/c2/24fbc25e1c64ffcac48baf3c50e8c61f.jpg', 'http://www.designbolts.com/wp-content/uploads/2015/07/low-poly-iphone-6-wallpaper.jpg', 'https://i.pinimg.com/originals/bd/ae/0a/bdae0aa380f1f33b9d5265ddc03e1ef2.jpg', 'http://cdn.wonderfulengineering.com/wp-content/uploads/2014/05/iPhone-wallpaper-45.jpg', 'http://theiphonewalls.com/wp-content/uploads/2013/08/Colorful-Geometry.jpg', 'https://i.pinimg.com/originals/da/f8/6b/daf86bbde0c5dcaaed7eeb80293521cc.jpg'],
-        index: 0
-    }
-   
-    // currently the same background image displays every time the app loads
-    // write a function to change the background image everytime screen is pressed
-    randomBackground = () => {
-        this.setState({
-            index : Math.floor(Math.random() * (7)),
-        })
-    };
-    
-    //Touchable highlight works, just added changeImage function, now need to connect it to touchable highlight
-    //also not sure if index is actually working
-    render() {
-        return (
-            <View style={styles.container}>
-                    
-            
-                    <ImageBackground
-                        style={styles.background}
-                        source={{uri: this.state.currentImg[this.state.index]}}
-                    > 
-                    <View>
-                        <TouchableHighlight
-                        onPress={this.randomBackground}>
-                            
-                            <Text style={styles.paragraph}>
-                                Touch to change image
-                            </Text>
-                        </TouchableHighlight>
-                    </View>
-                
-                    </ImageBackground>
-=======
     
 
     
@@ -74,7 +33,6 @@ export default class App extends Component {
                 <Text>you're accually broke.</Text>
             </View>
             </ImageBackground>
->>>>>>> 37224bdb8225b3b65ac5b0bb0c906ca30fa82bf0
             </View>
         );
     }
@@ -83,19 +41,6 @@ export default class App extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-<<<<<<< HEAD
-    },
-    background: {
-        flex: 1,
-        
-    },
-    paragraph: {
-        textAlign: 'center',
-        fontSize: 50,
-    }
-
-});
-=======
         justifyContent: 'center',
         textAlign: 'center',
         alignItems: 'center',
@@ -140,4 +85,3 @@ marginTop: 130,
     borderRadius: 10,
 },
 });
->>>>>>> 37224bdb8225b3b65ac5b0bb0c906ca30fa82bf0
