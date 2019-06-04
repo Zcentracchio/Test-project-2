70f6fe46e0736a2407596bb92fde01a7536ad42c
Zachary Centracchio <ZCentracchio@vt.ewsd.org>
2019-06-04 08:43:22 -0400

    final

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,56 +1,38 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, StyleSheet, Image, TouchableHighlight, ImageBackground } from 'react-native';
+import { AppRegistry, Text, View, StyleSheet, Image, TouchableHighlight, ImageBackground, Button} from 'react-native';
 import { Constants } from 'expo';
 
 export default class App extends Component {
     
-    state = {
-        backgroundToDisplay: '',
-    }
     
-    selectBackground = (timeOfDay) => {
-        if (timeOfDay == 'morning') {
-            this.setState({ 
-                backgroundToDisplay: '../assets/images/settings-Screen.png',
-            })
-        
-        } else if (timeOfDay == 'evening') {
-            this.setState({ 
-                backgroundToDisplay: '../assets/images/settings-Screen.png',
-            })
-        }
-    }
+
     
     render() {
         return (
             <View style={styles.container}>
-                <ImageBackground
-                    style={styles.background}
-                    source={{uri: this.state.backgroundToDisplay}}
+            <ImageBackground source={require('../assets/images/settings-Screen.png')} style={{width: '100%', height: '100%',}}>
+            <TouchableHighlight
+                    style={styles.button}
+                    onPress={() => { 
+                        alert('This should change the back ground but we never figured out how to do that.') 
+                    }}
                 >
+                    <Text style={styles.buttonText}>
+                        Change the Background
+                    </Text>
+                </TouchableHighlight>
+            <View style={styles.textContainer}>
                 
-                    <TouchableHighlight
-                        onPress={() => this.selectBackground('morning')}
-                    >
-                        <View style={styles.buttonView}>
-                            <Text style={styles.buttonText}>
-                                Good morning!
-                            </Text>
-                        </View>
-                    </TouchableHighlight>
-                    
-                    
-                    <TouchableHighlight
-                        onPress={() => this.selectBackground('evening')}
-                    >
-                        <View style={styles.buttonView}>
-                            <Text style={styles.buttonText}>
-                                Good evening!
-                            </Text>
-                        </View>
-                    </TouchableHighlight>
-                
-                </ImageBackground>
+                <Text>Terms of Service</Text>
+                <Text>This budgeting app is made for personal use only.</Text>
+                <Text>We do not condone Alabama's abortion laws.</Text>
+                <Text>Any sexual assault aligations will be treated</Text>
+                <Text>like a kid dropped their ice cream. </Text>
+                <Text>You may expieriance a rondom amount of money appear </Text>
+                <Text>in your account. This is Just to make you feel good.</Text>
+                <Text>you're accually broke.</Text>
+            </View>
+            </ImageBackground>
             </View>
         );
     }
@@ -61,24 +43,45 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         textAlign: 'center',
+        alignItems: 'center',
     },
     background: {
         flex: 1,
     },
     buttonView: {
-        height: 80,
-        width: 200,
+        height: 180,
+        width: 300,
         backgroundColor: 'black',
         justifyContent: 'center',
-
+        borderRadius: 10,
     },
     buttonText: {
-        fontSize: 30, 
-        color: 'white',
+        fontSize: 28, 
+        color: 'black',
         textAlign: 'center',
         justifyContent: 'center',
     },
-    ImageBackground:{
+    Text:{ 
+        fontSize: 2,
 
-    }
+    },
+   textContainer: {
+fontSize: 40,
+alignItems: 'center',
+justifyContent: 'space-around',
+marginTop: 130,
+   },
+   button: {
+    marginBottom: 10,
+    marginTop: 150,
+    marginLeft: 115,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'lightgreen',
+    height: 120,
+    width: 150,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 10,
+},
 });
